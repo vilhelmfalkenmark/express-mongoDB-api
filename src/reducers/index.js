ba27c4016e225123f0c5e7482332a82aad337f42
Vilhelm Falkenmark <vilhelmfalkenmark@gmail.com>
2017-04-14 21:36:03 +0200

    great stuff

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import data from "./ParticipantReducer";
+// import events from "./EventReducer";
+
+const reducer = combineReducers({
+  // data
+})
+
+export default reducer;
